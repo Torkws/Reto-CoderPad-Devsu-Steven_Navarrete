8f927ccd00cf377cbf7270227de3aebcf1a3faca
Torkws <47335736+Torkws@users.noreply.github.com>
2025-12-15 13:53:26 -0500

    New: Proyecto Karate Devsu

---
+++ AutoAPI-Opcion2/src/test/java/runnerTest.java
@@ -0,0 +1,7 @@
+import com.intuit.karate.junit5.Karate;
+public class runnerTest {
+    @Karate.Test
+    Karate testAll() {
+        return Karate.run("classpath:features").relativeTo(getClass());
+    }
+}
